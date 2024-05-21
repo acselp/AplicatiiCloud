0e2374219f37547e4fa9aaa0fb5fe232f918e459
Evgheniy Scofield <jenea9833@gmail.com>
2024-05-22 00:03:10 +0300

    test cvs

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MainController {
 
+
     @GetMapping("/")
     fun main(): String {
         return "Hello World!   fgfggf!"
